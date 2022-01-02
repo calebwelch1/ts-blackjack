c074e2973c25cf2a51841656aff440c3cb76964f
Bennington <caleb.welch11@gmail.com>
2022-01-02 12:13:45 -0300

    game logic

--- src/pages/Router.vue
+++ src/pages/Router.vue
@@ -4,8 +4,30 @@
   <div class="col-span-12 w-full">
     <div class="grid-col-12 w-full">
         <div class="col-span-12" style="background-color: black; height: 100vh; color: white;">
-        <button @click="click">click me :) </button>
-        {{getIndexByName('9 of Spades')}}
+          <div class="flex flex-col">
+            <div class="h-80vh w-60vw mx-auto" style="background-color:green; position: relative;">
+              <div class="w-24 h-8" style="position:absolute; background-color: black; top: 1rem; left: 35%; color: white;">
+              {{DealerCards}}
+              </div>
+              <div class="w-10 h-8" style="position:absolute; background-color: white; top: 45%; left: 5%; color:black">
+              <p> Money: </p>
+              <p>{{money}}</p>
+              </div>
+              <div class="w-24 h-8" style="position:absolute; background-color: white; top: 45%; left: 35%;">
+              </div>
+              <div class="w-10 h-8" style="position:absolute; background-color: white; top: 45%; right: 5%; color: black;">
+              <p> Bet: </p>
+              <p>{{bet}}</p>
+              </div>
+              <div class="w-24 h-8" style="position:absolute; background-color: black; bottom: 1rem; left: 35%; color: white;">
+              {{PlayerCards}}
+              </div>
+            </div>
+            <p>index by name: '2 of Spades' is : {{getIndexByName('Ace of Spades')}}</p>
+            <p>currentDeck length: {{currentDeck.length}}</p>
+            <p>randomCard: {{getRandomCard()}}</p>
+            <button @click="removeCard('9 of Spades')">click to remove a card by index name</button>
+          </div>
         </div>
         <div class="col-span-12" style="background-color: blue; height: 100vh; color: white;">
         </div>
@@ -27,6 +49,7 @@ type Card = {
     value: number,
     suit: string,
 }
+// 2 of spades is 0, ace of hearts is 51 = 52
 
 let deck: any[]=[];
 
@@ -53,26 +76,69 @@ for (let i = 0; i<cards.length; i++){
     deck.push(newCard);
   }
 }
+const currentDeck = deck;
 
 export default Vue.extend({
   name: 'HomeRouter',
   data() {
   return {
-    none: false,
+    gameSteps: ['bet', 'deal', 'game', 'result'],
+    gameStep: 0,
     message: 'hey there ;)',
     deck,
+    currentDeck,
+    bet: 0,
+    money: 100,
+    PlayerCards: [],
+    DealerCards: [],
   }
  },
+ mounted() {
+   switch(this.gameSteps[this.gameStep]) {
+  case 'bet':
+    console.log('you need to bet!')
+    break;
+  case 'deal':
+    // code block
+    break;
+  case 'game':
+    // code block
+    break;
+  case 'result':
+    // code block
+    break;
+  default:
+    console.log('plz return to game loop')
+}
+},
   methods: {
-   handleClick(tab, event) {
-     console.log(tab, event);
-   },
-   click(): void {
-     window.alert(this.message);
-   },
     getIndexByName(name: string): number{
-    return deck.map(function(e) { return e.name; }).indexOf(name);
-   }
+    return currentDeck.map(function(e) { return e.name; }).indexOf(name);
+   },
+  //  getIndexById(id: number): number{
+  //   return currentDeck.map(function(e) { return e.id; }).indexOf(id);
+  //  },
+    removeCard<T>(nameOfCard: T): Array<T> {
+    const index = currentDeck.map(function(e) { return e.name; }).indexOf(nameOfCard);
+    if (index > -1) {
+      currentDeck.splice(index, 1);
+    }
+  },
+    getRandomCard(){
+      // let namedDeck = currentDeck.map(function(e) { return e.name; });
+      let randomCard = currentDeck[Math.floor(Math.random() * currentDeck.length)];
+      return randomCard;
+    },
+    dealTwoCards(): Array {
+      // targetArray = [getRandomCard(), getRandomCard()];
+      let card1;
+      let card2;
+      card1 = this.getRandomCard();
+      this.removeCard(card1.name);
+      card2 = this.getRandomCard();
+      this.removeCard(card2.name);
+      return [card1, card2];
+    },
  },
  computed:{
  },
