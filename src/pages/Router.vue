f640be865acb9bcfafcd8b26374c3bef713b11e5
Bennington <caleb.welch11@gmail.com>
2022-01-06 20:58:16 -0300

    add cards and image logic

--- src/pages/Router.vue
+++ src/pages/Router.vue
@@ -18,7 +18,17 @@
           <div class="flex flex-col">
             <div class="h-80vh w-60vw mx-auto" style="background-color:green; position: relative;">
               <div class="w-24 h-8" style="position:absolute; background-color: black; top: 1rem; left: 35%; color: white;">
-              {{DealerCards}}
+                <div v-if="gameStep != 0" class="flex-row justify-between">
+                  <img
+                  :src="DealerCards[0].img"
+                  style="width: 40%;"
+                  />
+                  <img
+                  :src="DealerCards[1].img"
+                  style="width: 40%;"
+                  />
+                </div>
+                <p style="margin: 3rem;">{{DealerCards}}</p>
               </div>
               <div class="w-10 h-8" style="position:absolute; background-color: white; top: 45%; left: 5%; color:black">
               <p> Money: </p>
@@ -33,15 +43,26 @@
               <p>{{bet}}</p>
               </div>
               <div class="w-24 h-8" style="position:absolute; background-color: black; bottom: 1rem; left: 35%; color: white;">
-              {{PlayerCards}}
+              <div v-if="gameStep != 0" class="flex-row justify-between">
+                <img
+                :src="PlayerCards[0].img"
+                style="width: 40%;"
+                />
+                <img
+                :src="PlayerCards[1].img"
+                style="width: 40%;"
+                />
+              </div>
+                <p style="margin: 3rem;">{{PlayerCards}}</p>
+
               value:{{getValue(PlayerCards)}}
               </div>
             </div>
-            <p>index by name: '2 of Spades' is : {{getIndexByName('Ace of Spades')}}</p>
+            <p>index by name: 'two of Spades' is : {{getIndexByName('two_of_spades')}}</p>
             <p>currentDeck length: {{currentDeck.length}}</p>
             <p>Deck length: {{deck.length}}</p>
             <p>randomCard: {{getRandomCard()}}</p>
-            <button @click="removeCard('9 of Spades')">click to remove a card by index name</button>
+            <button @click="removeCard('2_of_spades')">click to remove a card by index name</button>
           </div>
         </div>
         <div class="col-span-12" style="background-color: blue; height: 100vh; color: white;">
@@ -53,10 +74,66 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
+import two_of_clubs from './cards/two_of_clubs.svg'
+import three_of_clubs from './cards/three_of_clubs.svg'
+import four_of_clubs from './cards/four_of_clubs.svg'
+import five_of_clubs from './cards/five_of_clubs.svg'
+import six_of_clubs from './cards/six_of_clubs.svg'
+import seven_of_clubs from './cards/seven_of_clubs.svg'
+import eight_of_clubs from './cards/eight_of_clubs.svg'
+import nine_of_clubs from './cards/nine_of_clubs.svg'
+import ten_of_clubs from './cards/ten_of_clubs.svg'
+import jack_of_clubs from './cards/jack_of_clubs.svg'
+import queen_of_clubs from './cards/queen_of_clubs.svg'
+import king_of_clubs from './cards/king_of_clubs.svg'
+import ace_of_clubs from './cards/ace_of_clubs.svg'
 
-let cards: any[]=[ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+import two_of_hearts from './cards/two_of_hearts.svg'
+import three_of_hearts from './cards/three_of_hearts.svg'
+import four_of_hearts from './cards/four_of_hearts.svg'
+import five_of_hearts from './cards/five_of_hearts.svg'
+import six_of_hearts from './cards/six_of_hearts.svg'
+import seven_of_hearts from './cards/seven_of_hearts.svg'
+import eight_of_hearts from './cards/eight_of_hearts.svg'
+import nine_of_hearts from './cards/nine_of_hearts.svg'
+import ten_of_hearts from './cards/ten_of_hearts.svg'
+import jack_of_hearts from './cards/jack_of_hearts.svg'
+import queen_of_hearts from './cards/queen_of_hearts.svg'
+import king_of_hearts from './cards/king_of_hearts.svg'
+import ace_of_hearts from './cards/ace_of_hearts.svg'
 
-const suits: string[]=['Spades', 'Clubs', 'Diamonds', 'Hearts'];
+import two_of_diamonds from './cards/two_of_diamonds.svg'
+import three_of_diamonds from './cards/three_of_diamonds.svg'
+import four_of_diamonds from './cards/four_of_diamonds.svg'
+import five_of_diamonds from './cards/five_of_diamonds.svg'
+import six_of_diamonds from './cards/six_of_diamonds.svg'
+import seven_of_diamonds from './cards/seven_of_diamonds.svg'
+import eight_of_diamonds from './cards/eight_of_diamonds.svg'
+import nine_of_diamonds from './cards/nine_of_diamonds.svg'
+import ten_of_diamonds from './cards/ten_of_diamonds.svg'
+import jack_of_diamonds from './cards/jack_of_diamonds.svg'
+import queen_of_diamonds from './cards/queen_of_diamonds.svg'
+import king_of_diamonds from './cards/king_of_diamonds.svg'
+import ace_of_diamonds from './cards/ace_of_diamonds.svg'
+
+import two_of_spades from './cards/two_of_spades.svg'
+import three_of_spades from './cards/three_of_spades.svg'
+import four_of_spades from './cards/four_of_spades.svg'
+import five_of_spades from './cards/five_of_spades.svg'
+import six_of_spades from './cards/six_of_spades.svg'
+import seven_of_spades from './cards/seven_of_spades.svg'
+import eight_of_spades from './cards/eight_of_spades.svg'
+import nine_of_spades from './cards/nine_of_spades.svg'
+import ten_of_spades from './cards/ten_of_spades.svg'
+import jack_of_spades from './cards/jack_of_spades.svg'
+import queen_of_spades from './cards/queen_of_spades.svg'
+import king_of_spades from './cards/king_of_spades.svg'
+import ace_of_spades from './cards/ace_of_spades.svg'
+
+
+let cards: any[]=[ 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
+
+const suits: string[]=['clubs', 'hearts', 'diamonds', 'spades'];
 
 type Card = {
     id: number,
@@ -64,7 +141,7 @@ type Card = {
     value: number,
     suit: string,
 }
-//TODO: (exceptions for aces!), when money is 0, ui lol, fix weird deck not losing or gaining cards, probably just put making the deck into it's own method.
+//TODO: (exceptions for aces!), when money is 0, ui lol,
 
 let deck: any[]=[];
 
@@ -88,6 +165,60 @@ export default Vue.extend({
     HitStay: false,
     result: '',
     showResultText: false,
+    cardImgs:[
+    two_of_clubs,
+    two_of_hearts,
+    two_of_diamonds,
+    two_of_spades,
+    three_of_clubs,
+    three_of_hearts,
+    three_of_diamonds,
+    three_of_spades,
+    four_of_clubs,
+    four_of_hearts,
+    four_of_diamonds,
+    four_of_spades,
+    five_of_clubs,
+    five_of_hearts,
+    five_of_diamonds,
+    five_of_spades,
+    six_of_clubs,
+    six_of_hearts,
+    six_of_diamonds,
+    six_of_spades,
+    seven_of_clubs,
+    seven_of_hearts,
+    seven_of_diamonds,
+    seven_of_spades,
+    eight_of_clubs,
+    eight_of_hearts,
+    eight_of_diamonds,
+    eight_of_spades,
+    nine_of_clubs,
+    nine_of_hearts,
+    nine_of_diamonds,
+    nine_of_spades,
+    ten_of_clubs,
+    ten_of_hearts,
+    ten_of_diamonds,
+    ten_of_spades,
+    jack_of_clubs,
+    jack_of_hearts,
+    jack_of_diamonds,
+    jack_of_spades,
+    queen_of_clubs,
+    queen_of_hearts,
+    queen_of_diamonds,
+    queen_of_spades,
+    king_of_clubs,
+    king_of_hearts,
+    king_of_diamonds,
+    king_of_spades,
+    ace_of_clubs,
+    ace_of_hearts,
+    ace_of_diamonds,
+    ace_of_spades,
+    ],
   }
  },
  mounted() {
@@ -209,12 +340,21 @@ export default Vue.extend({
       this.gameStep++ ;
       this.gameloop();
     },
+    addImgs(){
+      for (let i=0; i<this.currentDeck.length; i++){
+        this.currentDeck[i].img = this.cardImgs[i];
+      }
+    },
     gameloop(){
       switch(this.gameSteps[this.gameStep]) {
   case 'bet':
     console.log('you need to bet!');
     this.currentDeck = [];
     this.makeDeck(this.currentDeck);
+    this.addImgs();
+    console.log('img',this.currentDeck[0].img);
+    console.log('cardimgs',this.cardImgs);
+    console.log('deck',this.currentDeck);
     console.log(this.currentDeck.length);
     document.getElementById("myModal").style.display="block";
     break;
@@ -241,23 +381,70 @@ export default Vue.extend({
     this.gameloop();
   },
   makeDeck(deck: Array): Array{
+
     for (let i = 0; i<cards.length; i++){
 
       for (let j=0; j<suits.length; j++){
 
         let currentValue;
 
-        if (cards[i] === 'Jack' || cards[i] === 'Queen' || cards[i] === 'King'){
-          currentValue = 10;
-        }
-        else if (cards[i] === 'Ace'){
-          currentValue = 11;
-        }
-        else currentValue = cards[i];
-
+        // if (cards[i] === 'jack' || cards[i] === 'queen' || cards[i] === 'king'){
+        //   currentValue = 10;
+        // }
+        // else if (cards[i] === 'ace'){
+        //   currentValue = 11;
+        // }
+        // else currentValue = cards[i];
+        switch(cards[i]) {
+              case 'one':
+              currentValue = 1;
+              break;
+              case 'two':
+              currentValue = 2;
+              break;
+              case 'three':
+              currentValue = 3;
+              break;
+              case 'four':
+              currentValue = 4;
+              break;
+              case 'five':
+              currentValue = 5;
+              break;
+              case 'six':
+              currentValue = 6;
+              break;
+              case 'seven':
+              currentValue = 7;
+              break;
+              case 'eight':
+              currentValue = 8;
+              break;
+              case 'nine':
+              currentValue = 9;
+              break;
+              case 'ten':
+              currentValue = 10;
+              break;
+              case 'jack':
+              currentValue = 10;
+              break;
+              case 'queen':
+              currentValue = 10;
+              break;
+              case 'king':
+              currentValue = 10;
+              break;
+              case 'ace':
+              currentValue = 11;
+              break;
+              default:
+                console.log('you broke the deck')
+            }
+            let newName = cards[i].toString() + `_of_` + suits[j];
         let newCard: Card = {
           id: cards[i],
-          name: cards[i].toString() + ` of ` + suits[j],
+          name: newName,
           value: currentValue,
           suit: suits[j],
         }
